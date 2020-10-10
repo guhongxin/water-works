2f221b3b4afaf64bb41e486577b6b74e87ec69e9
guhongxin <1204738681@qq.com>
2020-10-10 19:16:45 +0800

    水管定位

--- js/index.js
+++ js/index.js
@@ -13,4 +13,121 @@ $(function() {
     $('.crew').eq(_index).css('display', 'block')
     console.log(_index)
   })
+  // 获取屏幕分辨率
+  let screenWidth = window.screen.width
+  if (screenWidth >= 1920 && screenWidth <= 2560) {
+    $('#bearing .part-two  .table-list > .table:first-child .table-head .table-tr .table-th').css({
+      'height': '60px',
+      'line-height': '60px'
+    })
+    $('#bearing .part-two').css({
+        'width': '95%',
+        'position': 'absolute',
+        'top': '65%',
+        'left': '50%',
+        'transform': 'translateX(-50%)'
+      })
+    $('#bearing .part-two  .table-list > .table:not(:first-child) .table-head .table-tr .table-th').css({
+      'height': '30px',
+      'line-height': '30px'
+    })
+    $('#bearing .table .table-body .table-tr .table-td').css({
+      'height': '30px',
+      'width': '100%',
+      'line-height': '30px'
+    })
+    $('#bearing .table-footer .details').css({
+      'text-align': 'center',
+      'height': '30px',
+      'line-height': '30px'
+    })
+    $('#bearing .part-one .part-one-middle .crew .bearing').css({
+      'font-size': '12px'
+    })
+    $('#bearing .table .table-tr .table-td').css({
+      'font-size': '12px'
+    })
+    $('#bearing .table .table-tr .table-th').css({
+      'font-size': '12px'
+    })
+    $('#bearing .table-footer .details').css({
+      'font-size': '12px'
+    })
+  } else if (screenWidth > 2560) {
+    $('#bearing .part-two  .table-list > .table:first-child .table-head .table-tr .table-th').css({
+      'height': '100px',
+      'line-height': '100px'
+    })
+    $('#bearing .part-two').css({
+        'width': '95%',
+        'position': 'absolute',
+        'top': '65%',
+        'left': '50%',
+        'transform': 'translateX(-50%)'
+      })
+    $('#bearing .part-two  .table-list > .table:not(:first-child) .table-head .table-tr .table-th').css({
+      'height': '50px',
+      'line-height': '50px'
+    })
+    $('#bearing .table .table-body .table-tr .table-td').css({
+      'height': '50px',
+      'width': '100%',
+      'line-height': '50px'
+    })
+    $('#bearing .table-footer .details').css({
+      'text-align': 'center',
+      'height': '50px',
+      'line-height': '50px'
+    })
+    $('#bearing .part-one .part-one-middle .crew .bearing').css({
+      'font-size': '16px'
+    })
+    $('#bearing .table .table-tr .table-td').css({
+      'font-size': '16px'
+    })
+    $('#bearing .table .table-tr .table-th').css({
+      'font-size': '16px'
+    })
+    $('#bearing .table-footer .details').css({
+      'font-size': '16px'
+    })
+  } else {
+    $('#bearing .part-two  .table-list > .table:first-child .table-head .table-tr .table-th').css({
+      'height': '60px',
+      'line-height': '60px'
+    })
+    $('#bearing .part-two').css({
+      'width': '95%',
+      'position': 'absolute',
+      'top': '58%',
+      'left': '50%',
+      'transform': 'translateX(-50%)'
+    })
+    $('#bearing .part-two  .table-list > .table:not(:first-child) .table-head .table-tr .table-th').css({
+      'height': '30px',
+      'line-height': '30px'
+    })
+    $('#bearing .table .table-body .table-tr .table-td').css({
+      'height': '30px',
+      'width': '100%',
+      'line-height': '30px'
+    })
+    $('#bearing .table-footer .details').css({
+      'text-align': 'center',
+      'height': '30px',
+      'line-height': '30px'
+    })
+    $('#bearing .part-one .part-one-middle .crew .bearing').css({
+      'font-size': '12px'
+    })
+    $('#bearing .table .table-tr .table-td').css({
+      'font-size': '12px'
+    })
+    $('#bearing .table .table-tr .table-th').css({
+      'font-size': '12px'
+    })
+    $('#bearing .table-footer .details').css({
+      'font-size': '12px'
+    })
+  }
 })
--- js/networkTopology.js
+++ js/networkTopology.js
@@ -1,9 +1,9 @@
 $(function() {
   // 加载完成
-  imgRatio(setMeter)
+  imgRatio(setMeter0)
 })
-//定位仪表
-function setMeter() {
+//1920定位仪表
+function setMeter0() {
   $('.instrumentlocation .meter').css('height', '76px')
   $('.instrumentlocation1').css({top: '31%', left: '18%' })
   $('.instrumentlocation2').css({top: '91%', left: '18%' })
@@ -38,6 +38,79 @@ function setMeter() {
   $('.east-gd-details').css({top: '15%', left: '16%' })
   $('.west-gd-details').css({top: '75%', left: '17%' })
 }
+//1730定位仪表
+function setMeter1() {
+  $('.instrumentlocation .meter').css('height', '78px')
+  $('.instrumentlocation1').css({top: '30%', left: '18%' })
+  $('.instrumentlocation2').css({top: '90%', left: '18%' })
+  $('.instrumentlocation3').css({top: '4.6%', left: '31.3%' })
+  $('.instrumentlocation4').css({top: '21.5%', left: '31.3%' })
+  $('.instrumentlocation5').css({top: '38.6%', left: '31.3%' })
+  $('.instrumentlocation6').css({top: '55.8%', left: '31.3%' })
+  $('.instrumentlocation7').css({top: '72.7%', left: '31.3%' })
+  $('.instrumentlocation8').css({top: '89.6%', left: '31.3%' })
+
+  $('.instrumentlocation9').css({top: '4%', left: '43.3%' })
+  $('.instrumentlocation10').css({top: '21%', left: '43.3%' })
+  $('.instrumentlocation11').css({top: '38%', left: '43.3%' })
+  $('.instrumentlocation12').css({top: '55%', left: '43.3%' })
+  $('.instrumentlocation13').css({top: '72%', left: '43.3%' })
+  $('.instrumentlocation14').css({top: '89%', left: '43.3%' })
+
+  $('.instrumentlocation15').css({top: '4%', left: '57.6%' })
+  $('.instrumentlocation16').css({top: '21%', left: '57.6%' })
+  $('.instrumentlocation17').css({top: '38%', left: '57.6%' })
+  $('.instrumentlocation18').css({top: '55%', left: '57.6%' })
+  $('.instrumentlocation19').css({top: '72%', left: '57.6%' })
+  $('.instrumentlocation20').css({top: '89%', left: '57.6%' })
+  
+  $('.instrumentlocation21').css({top: '4%', left: '71.8%' })
+  $('.instrumentlocation22').css({top: '21%', left: '71.8%' })
+  $('.instrumentlocation23').css({top: '38%', left: '71.8%' })
+  $('.instrumentlocation24').css({top: '55%', left: '71.8%' })
+  $('.instrumentlocation25').css({top: '72%', left: '71.8%' })
+  $('.instrumentlocation26').css({top: '89%', left: '71.8%' })
+
+  $('.east-gd-details').css({top: '14%', left: '16%' })
+  $('.west-gd-details').css({top: '74%', left: '17%' })
+}
+
+//1730定位仪表
+function setMeter2() {
+  $('.instrumentlocation .meter').css('height', '60px')
+  $('.instrumentlocation1').css({top: '29%', left: '18%' })
+  $('.instrumentlocation2').css({top: '89%', left: '18%' })
+  $('.instrumentlocation3').css({top: '3.4%', left: '31.2%' })
+  $('.instrumentlocation4').css({top: '20.4%', left: '31.2%' })
+  $('.instrumentlocation5').css({top: '37.5%', left: '31.2%' })
+  $('.instrumentlocation6').css({top: '55%', left: '31.2%' })
+  $('.instrumentlocation7').css({top: '71.7%', left: '31.2%' })
+  $('.instrumentlocation8').css({top: '89%', left: '31.2%' })
+
+  $('.instrumentlocation9').css({top: '4%', left: '42.4%' })
+  $('.instrumentlocation10').css({top: '21%', left: '42.4%' })
+  $('.instrumentlocation11').css({top: '38%', left: '42.4%' })
+  $('.instrumentlocation12').css({top: '55%', left: '42.4%' })
+  $('.instrumentlocation13').css({top: '72%', left: '42.4%' })
+  $('.instrumentlocation14').css({top: '89%', left: '42.4%' })
+
+  $('.instrumentlocation15').css({top: '4%', left: '56.6%' })
+  $('.instrumentlocation16').css({top: '21%', left: '56.6%' })
+  $('.instrumentlocation17').css({top: '38%', left: '56.6%' })
+  $('.instrumentlocation18').css({top: '55%', left: '56.6%' })
+  $('.instrumentlocation19').css({top: '72%', left: '56.6%' })
+  $('.instrumentlocation20').css({top: '89%', left: '56.6%' })
+  
+  $('.instrumentlocation21').css({top: '4%', left: '70.8%' })
+  $('.instrumentlocation22').css({top: '21%', left: '70.8%' })
+  $('.instrumentlocation23').css({top: '38%', left: '70.8%' })
+  $('.instrumentlocation24').css({top: '55%', left: '70.8%' })
+  $('.instrumentlocation25').css({top: '72%', left: '70.8%' })
+  $('.instrumentlocation26').css({top: '89%', left: '70.8%' })
+
+  $('.east-gd-details').css({top: '6%', left: '13%' })
+  $('.west-gd-details').css({top: '65%', left: '15%' })
+}
 // 获取图片比率
 function imgRatio(callback) {
   let img = new Image()
@@ -55,6 +128,17 @@ function imgRatio(callback) {
     if (waterworkimgwt >= wt) {
       $('.water-network').css('width', wt + 'px')
       callback()
+    } else {
+      // 获取屏幕分辨率
+      let screenWidth = window.screen.width
+      if (screenWidth >= 1920) {
+        $('.water-network').css('width', '1730px')
+        setMeter1()
+      } else {
+        $('.water-network').css('width', '1200px')
+        setMeter2()
+        console.log('screenWidth', screenWidth)
+      }
     }
   }
 }
